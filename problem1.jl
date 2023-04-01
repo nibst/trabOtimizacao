201ac0a7ddbd930b104528922cf7fae6efec5f85
nikolas <nikolasps7@gmail.com>
2023-04-01 18:14:12 -0300

    funcionando pra todas instancias sem ser a de 10k

--- problem1.jl
+++ problem1.jl
@@ -1,5 +1,6 @@
 using JuMP
 using GLPK
+using Graphs
 
 function isPerfectSquare(n::Int)
     if (ceil(sqrt(n)) == floor(sqrt(n)))
@@ -13,7 +14,7 @@ struct Graph
     vertices::Vector{Int}
     edges::Vector{Tuple{Int,Int}}
 end
-function reader(filepath :: String)
+function createGraph(filepath :: String)
     f = open(filepath, "r")
     lines = readlines(f)
     instanceLenght = parse(Int, lines[1])
@@ -33,5 +34,58 @@ function reader(filepath :: String)
     end
     return Graph(vertices,edges)
 end
-path = "./instances/instance_20.dat"
-graph = reader(path)
+
+path = "./instances/instance_1000.dat"
+graph = createGraph(path)
+f = open(path, "r")
+lines = readlines(f)
+instanceLenght = parse(Int, lines[1])
+close(f)
+
+model = Model(GLPK.Optimizer);
+
+# Definindo as variáveis do modelo
+n_vertices = instanceLenght  # número de vértices do grafo
+@variable(model, x[i=1:n_vertices, j=1:n_vertices], Bin)
+
+@objective(model, Max, sum(x));
+
+# Definindo as restrições do modelo
+for i in 1:n_vertices
+    @constraint(model, sum(x[j, i] for j in 1:n_vertices if (j, i) in graph.edges) + 
+        sum(x[i, j] for j in 1:n_vertices if (i, j) in graph.edges) <= 2)
+    @constraint(model,sum(x[j, i] for j in 1:n_vertices if !((j, i) in graph.edges)) + 
+    sum(x[i, j] for j in 1:n_vertices if !((i, j) in graph.edges)) == 0)
+end
+
+
+# Create an empty graph
+g = SimpleGraph(n_vertices)
+
+# Add the edges to the graph
+for e in graph.edges
+    add_edge!(g, e[1], e[2])
+end
+
+cycles = cycle_basis(g)
+
+# constraint pra impedir ciclos
+for cycle in cycles
+    cycle_edges = [(cycle[i], cycle[i+1]) for i=1:length(cycle)-1]
+    cycle_edges = union(cycle_edges, [(cycle[end], cycle[1])])
+    @constraint(model, sum(x[min(i,j),max(i,j)] for (i,j) in cycle_edges) <= length(cycle) - 1)
+end
+#println(model)
+optimize!(model)
+termination_status(model)
+
+println("Solução:")
+println(value(sum(x)))
+
+for i in 1:n_vertices
+    for j in 1:n_vertices
+        if value(x[i, j]) == 1
+            #println("Aresta de $i para $j está presente.")
+        end
+    end
+end
